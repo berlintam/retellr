b56b603fb205a35dc5cef4aa388ae863397440e8
tam <tam.eastley@gmail.com>
2014-05-29 13:24:59 +0200

    basic structure

---
+++ retellr.rb
@@ -0,0 +1,42 @@
+prompt = 'Who are you in this convo? '
+print prompt
+user = STDIN.gets.chomp
+puts '-------------'
+
+text = File.open('chat.txt')
+
+bullshit = [
+  "-- are you kidding me?",
+  "-- whaaaaaat?",
+  "-- that's crazy!",
+  "-- really??? Like... seriously???",
+  "--- totes"
+]
+
+me = [
+  "I was all like",
+  "Then I said",
+  "What happened then... I said something like"
+]
+
+def random_bullshit(bullshit)
+  puts bullshit.sample if rand > 0.6
+end
+
+def random_me(me)
+  puts me.sample
+end
+
+text.each do |line|
+  line = line[20..-1]
+  name, msg = line.split(/:/)
+  if name == user
+    puts "#{random_me(me)} #{msg}"
+    random_bullshit(bullshit)
+  else
+    puts "#{name} was all like #{msg}"
+    random_bullshit(bullshit)
+  end
+end
+
+
